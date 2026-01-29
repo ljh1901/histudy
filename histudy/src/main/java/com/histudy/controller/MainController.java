cf4fd7c96e25ef9a42731225b1d0d9dc5c473712
ljh1901 <juho8690@gmail.com>
2026-01-30 00:26:05 +0900

    hotfix -- applicationContext, 비밀번호 암호화, controller패키지 예시

---
+++ histudy/src/main/java/com/histudy/controller/MainController.java
@@ -0,0 +1,12 @@
+package com.histudy.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+	@GetMapping("/index.do")
+	public String mainView() {
+		return "main";
+	}
+}
