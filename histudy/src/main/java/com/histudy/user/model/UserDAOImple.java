16718ec10e419af9f73dbc4520b6a2e909c7fdee
ljh1901 <juho8690@gmail.com>
2026-02-02 09:37:31 +0900

    userSignUp
    Removed unused methods and kept only userSignUp.

--- histudy/src/main/java/com/histudy/user/model/UserDAOImple.java
+++ histudy/src/main/java/com/histudy/user/model/UserDAOImple.java
@@ -12,8 +12,6 @@ public class UserDAOImple implements UserDAO {
 
 	private static final String NAMESPACE = "com.histudy.user";
 
-	// 1. 이 메서드 하나만 남기고 나머지는 지우세요!
-	@Override
 	public void userSignUp(UserDTO dto) {
 		sqlSession.insert(NAMESPACE + ".userSignupId", dto);
 	}   
