60390559474d2a2e0ffe489f4a2919afd3e965f2
ljh1901 <juho8690@gmail.com>
2026-02-05 03:20:12 +0900

    user.js

--- histudy/src/main/webapp/js/user/user.js
+++ histudy/src/main/webapp/js/user/user.js
@@ -21,11 +21,19 @@
 	
 	}
 
-function getCookie(name) {
-    return document.cookie
-        .split("; ")
-        .find(row => row.startsWith(name + "="))
-        ?.split("=")[1];
+function getCookie(name){
+
+    const cookies = document.cookie.split(";");
+
+    for(let i=0; i<cookies.length; i++){
+
+        const cookie = cookies[i].trim();
+
+        if(cookie.indexOf(name + "=") === 0){
+            return cookie.substring(name.length + 1);
+        }
+    }
+    return null;
 }
 
 function openSignInModal() {
