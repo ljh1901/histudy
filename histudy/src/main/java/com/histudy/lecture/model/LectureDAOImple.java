97e4a0c86e6936b88282dd59e8f842873e874ea2
ljh1901 <juho8690@gmail.com>
2026-02-04 15:17:33 +0900

    Update LectureDAOImple.java

--- histudy/src/main/java/com/histudy/lecture/model/LectureDAOImple.java
+++ histudy/src/main/java/com/histudy/lecture/model/LectureDAOImple.java
@@ -5,16 +5,20 @@ import java.util.List;
 import org.mybatis.spring.SqlSessionTemplate;
 
 public class LectureDAOImple implements LectureDAO {
-	private SqlSessionTemplate sqlSession;
-	public LectureDAOImple(SqlSessionTemplate sqlSession) {
-		this.sqlSession=sqlSession;
-	}
-	@Override
-	public List<LectureDTO> lectureList() {
-		return sqlSession.selectList("com.histudy.lecture.lectureList");
-	}
-	@Override
-	public LectureDTO lectureContent(int idx) {
-		return sqlSession.selectOne("com.histudy.lecture.lectureList",idx);
-	}
+   private SqlSessionTemplate sqlSession;
+   public LectureDAOImple(SqlSessionTemplate sqlSession) {
+      this.sqlSession=sqlSession;
+   }
+   @Override
+   public List<LectureDTO> lectureList() {
+      return sqlSession.selectList("com.histudy.lecture.lectureList");
+   }
+   @Override
+   public LectureDTO lectureContent(int idx) {
+      return sqlSession.selectOne("com.histudy.lecture.lectureContent",idx);
+   }
+   @Override
+   public int lectureNoteInsert(LectureNoteDTO dto) {
+      return sqlSession.insert("com.histudy.lecture.lectureNoteInsert",dto);
+   }
 }
