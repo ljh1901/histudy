4bc128a63cc5284ac22ec0113d1cf9ebc98c55ea
ljh1901 <juho8690@gmail.com>
2026-02-04 15:07:28 +0900

    Update StudyDAOImple.java

--- histudy/src/main/java/com/histudy/study/model/StudyDAOImple.java
+++ histudy/src/main/java/com/histudy/study/model/StudyDAOImple.java
@@ -1,27 +1,48 @@
 package com.histudy.study.model;
 
 import java.util.List;
+import java.util.Map;
 
 import org.mybatis.spring.SqlSessionTemplate;
 
-public class StudyDAOImple implements StudyDAO {
-	
-	private SqlSessionTemplate sqlSession;
-	
-	public StudyDAOImple(SqlSessionTemplate sqlSession) {
-		super();
-		this.sqlSession = sqlSession;
-	}
+import com.histudy.user.model.UserDTO;
 
-	@Override
-	public int createStudy(StudyDTO dto) {
-		int count = sqlSession.insert("insertStudy", dto);
-		return count;
-	}
-	
-	@Override
-	public List<StudyDTO> getStudyList() {
-		List<StudyDTO> lists = sqlSession.selectList("selectStudyList");
-		return lists;
-	}
+public class StudyDAOImple implements StudyDAO {
+   
+   private SqlSessionTemplate sqlSession;
+   
+   public StudyDAOImple(SqlSessionTemplate sqlSession) {
+      super();
+      this.sqlSession = sqlSession;
+   }
+
+   @Override
+   public int createStudy(StudyDTO dto) {
+      int count = sqlSession.insert("insertStudy", dto);
+      return count;
+   }
+   
+   @Override
+   public List<StudyDTO> getStudyList(Map<String, Integer> map) {
+      List<StudyDTO> lists = sqlSession.selectList("selectStudyList", map);
+      return lists;
+   }
+   
+   @Override
+   public int studyTotalCnt() {
+      int count = sqlSession.selectOne("studyTotalCnt");
+      return count;
+   }
+   
+   @Override
+   public UserDTO getStudyCreateUser(int user_idx) {
+      UserDTO dto = sqlSession.selectOne("studyCreateUser", user_idx);
+      return dto;
+   }
+   
+   @Override
+   public int studyMaxCreate(int user_idx) {
+      int result = sqlSession.selectOne("studyMaxCreate", user_idx);
+      return result;
+   }
 }
