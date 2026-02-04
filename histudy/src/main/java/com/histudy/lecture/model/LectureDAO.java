96e97c13cd7ad686119bdbf0026dd37da7ef8408
ljh1901 <juho8690@gmail.com>
2026-02-04 15:17:16 +0900

    Update LectureDAO.java

--- histudy/src/main/java/com/histudy/lecture/model/LectureDAO.java
+++ histudy/src/main/java/com/histudy/lecture/model/LectureDAO.java
@@ -3,6 +3,8 @@ package com.histudy.lecture.model;
 import java.util.List;
 
 public interface LectureDAO {
-	public List<LectureDTO> lectureList();
-	public LectureDTO lectureContent(int idx);
+   public List<LectureDTO> lectureList();
+   public LectureDTO lectureContent(int idx);
+   public int lectureNoteInsert(LectureNoteDTO dto);
+
 }
