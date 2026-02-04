846e42350df5ac2bb0a6a8f81fdbdbc60a548758
ljh1901 <juho8690@gmail.com>
2026-02-04 15:20:31 +0900

    Update LectureServiceImple.java

--- histudy/src/main/java/com/histudy/lecture/service/LectureServiceImple.java
+++ histudy/src/main/java/com/histudy/lecture/service/LectureServiceImple.java
@@ -7,17 +7,22 @@ import org.springframework.stereotype.Service;
 
 import com.histudy.lecture.model.LectureDAO;
 import com.histudy.lecture.model.LectureDTO;
+import com.histudy.lecture.model.LectureNoteDTO;
 public class LectureServiceImple implements LectureService {
-	private LectureDAO lectureDao;
-	public LectureServiceImple(LectureDAO lectureDao) {
-		this.lectureDao=lectureDao;
-	}
-	@Override
-	public List<LectureDTO> lectureList() {
-		return lectureDao.lectureList();
-	}
-	@Override
-	public LectureDTO lectureContent(int idx) {
-		return lectureDao.lectureContent(idx);
-	}
+   private LectureDAO lectureDao;
+   public LectureServiceImple(LectureDAO lectureDao) {
+      this.lectureDao=lectureDao;
+   }
+   @Override
+   public List<LectureDTO> lectureList() {
+      return lectureDao.lectureList();
+   }
+   @Override
+   public LectureDTO lectureContent(int idx) {
+      return lectureDao.lectureContent(idx);
+   }
+   @Override
+   public int lectureNoteInsert(LectureNoteDTO dto) {
+      return lectureDao.lectureNoteInsert(dto);
+   }
 }
