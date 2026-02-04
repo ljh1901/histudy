661aac23c32e75f9898941c376787949d74455c9
ljh1901 <juho8690@gmail.com>
2026-02-04 15:07:48 +0900

    Update StudyServiceImple.java

--- histudy/src/main/java/com/histudy/study/service/StudyServiceImple.java
+++ histudy/src/main/java/com/histudy/study/service/StudyServiceImple.java
@@ -1,28 +1,48 @@
 package com.histudy.study.service;
 
 import java.util.List;
+import java.util.Map;
 
 import com.histudy.study.model.*;
+import com.histudy.user.model.UserDTO;
 
 public class StudyServiceImple implements StudyService {
 
-	private StudyDAO studyDao;
-	
-	public StudyServiceImple(StudyDAO studyDao) {
-		super();
-		this.studyDao = studyDao;
-	}
+   private StudyDAO studyDao;
+   
+   public StudyServiceImple(StudyDAO studyDao) {
+      super();
+      this.studyDao = studyDao;
+   }
 
-	@Override
-	public int createStudy(StudyDTO dto) {
-		int result = studyDao.createStudy(dto);
-		return result;
-	}
-	
-	@Override
-	public List<StudyDTO> getStudyList() {
-		List<StudyDTO> lists = studyDao.getStudyList();
-		return lists;
-	}
+   @Override
+   public int createStudy(StudyDTO dto) {
+      int result = studyDao.createStudy(dto);
+      return result;
+   }
+   
+   @Override
+   public List<StudyDTO> getStudyList(Map<String, Integer> map) {
+      List<StudyDTO> lists = studyDao.getStudyList(map);
+      return lists;
+   }
+   
+   @Override
+   public int studyTotalCnt() {
+      int result = studyDao.studyTotalCnt();
+      return result;
+   }
+   
+   @Override
+   public UserDTO getStudyCreateUser(int user_idx) {
+      UserDTO dto = studyDao.getStudyCreateUser(user_idx);
+      return dto;
+   }
+   
+   @Override
+   public int studyMaxCreate(int user_idx) {
+      int result = studyDao.studyMaxCreate(user_idx);
+      return result;
+   }
 
 }
