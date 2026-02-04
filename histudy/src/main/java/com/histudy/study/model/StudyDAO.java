e2d1e264e388f211964776aa0c46af65fc42de6c
ljh1901 <juho8690@gmail.com>
2026-02-04 15:12:32 +0900

    Update StudyDAO.java

--- histudy/src/main/java/com/histudy/study/model/StudyDAO.java
+++ histudy/src/main/java/com/histudy/study/model/StudyDAO.java
@@ -1,10 +1,14 @@
 package com.histudy.study.model;
 
 import java.util.*;
+import com.histudy.user.model.*;
 
 public interface StudyDAO {
-	
-	public int createStudy(StudyDTO dto);
-	public List<StudyDTO> getStudyList();
+   
+   public int createStudy(StudyDTO dto);
+   public List<StudyDTO> getStudyList(Map<String, Integer> map);
+   public int studyTotalCnt();
+   public UserDTO getStudyCreateUser(int user_idx);
+   public int studyMaxCreate(int user_idx);
 
 }
