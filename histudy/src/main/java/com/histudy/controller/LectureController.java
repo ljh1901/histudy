2e095a02e6ca69e5671594dad3c1670a149c6d64
ljh1901 <juho8690@gmail.com>
2026-02-04 15:16:48 +0900

    Update LectureController.java

--- histudy/src/main/java/com/histudy/controller/LectureController.java
+++ histudy/src/main/java/com/histudy/controller/LectureController.java
@@ -8,30 +8,40 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.histudy.lecture.model.LectureDTO;
+import com.histudy.lecture.model.LectureNoteDTO;
 import com.histudy.lecture.service.LectureService;
 
 
 @Controller
 public class LectureController {
 
-	@Autowired
+   @Autowired
     private LectureService lectureService;
-	
-	@GetMapping("lecture.do")
-	public ModelAndView lectureList() {
-		ModelAndView mav= new ModelAndView();
-		List<LectureDTO> lists=lectureService.lectureList();
-		mav.addObject("lectureLists",lists);
-		mav.setViewName("lecture/lectureList");
-		
-		return mav;
-	}
-	@GetMapping("lectureContent.do")
-	public ModelAndView lectrueContent(int idx) {
-		LectureDTO list=lectureService.lectureContent(idx);
-		ModelAndView mav=new ModelAndView();
-		mav.addObject("lectureContent",list);
-		mav.setViewName("lecture/lectureContent");
-		return mav;
-	}
+   
+   @GetMapping("lecture.do")
+   public ModelAndView lectureList() {
+      ModelAndView mav= new ModelAndView();
+      List<LectureDTO> lists=lectureService.lectureList();
+      mav.addObject("lectureLists",lists);
+      mav.setViewName("lecture/lectureList");
+      
+      return mav;
+   }
+   @GetMapping("lectureContent.do")
+   public ModelAndView lectrueContent(int idx) {
+      LectureDTO list=lectureService.lectureContent(idx);
+      ModelAndView mav=new ModelAndView();
+      mav.addObject("lectureContent",list);
+      mav.setViewName("lecture/lectureContent");
+      return mav;
+   }
+   @GetMapping("lectureMemo.do")
+   public ModelAndView lectrueMemo(LectureNoteDTO dto) {
+      ModelAndView mav=new ModelAndView();
+      int result=lectureService.lectureNoteInsert(dto);
+      String msg=result>0?"메모 저장 성공.":"메모 저장 실패.";
+      mav.addObject("msg",msg);
+      mav.setViewName("lecture/lectureMemoOk");
+      return mav;
+   }
 }
