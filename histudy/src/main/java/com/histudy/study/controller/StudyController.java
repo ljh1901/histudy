6a9cf6669a18af6691cf42d69490bd45fc42bdee
ljh1901 <juho8690@gmail.com>
2026-02-04 15:06:07 +0900

    Update StudyController.java

--- histudy/src/main/java/com/histudy/study/controller/StudyController.java
+++ histudy/src/main/java/com/histudy/study/controller/StudyController.java
@@ -3,8 +3,6 @@ package com.histudy.study.controller;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.util.Date;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,60 +10,91 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
-
 import com.histudy.study.model.StudyDTO;
 import com.histudy.study.service.StudyService;
+import com.histudy.user.model.UserDTO;
+
 import java.util.*;
 
 @Controller
 public class StudyController {
 
-	@Autowired
-	private StudyService ss;
-	
-	@GetMapping("/studyList.do")
-	public ModelAndView studyList() {
-		
-		List<StudyDTO> lists = ss.getStudyList();
-		ModelAndView mav = new ModelAndView();
-		mav.addObject("studyList", lists);
-		mav.addObject("studyListCount", lists.size());
-		mav.setViewName("study/studyList");
-		return mav;
-	}
-	
-	@GetMapping("/studyCreateForm.do")
-	public String studyCreateForm() {
-		return "study/studyCreate";
-	}
-	
-	@PostMapping("/studyCreate.do")
-	public ModelAndView studyCreate(StudyDTO dto, MultipartFile rstudy_upload_img) {
-		
-		dto.setStudy_upload_img(rstudy_upload_img.getOriginalFilename());
-		fileCopy(rstudy_upload_img);
-		int result = ss.createStudy(dto);
-		String msg = result>0?"½ºÅÍµð °³¼³ ¿Ï·á":"½ºÅÍµð °³¼³ ½ÇÆÐ";
-		ModelAndView mav = new ModelAndView();
-		mav.addObject("msg", msg);
-		mav.setViewName("study/studyMsg");
-		return mav;
-	}
-	
-	public void fileCopy(MultipartFile upload) {
-		try {
-			byte bytes[] = upload.getBytes();
-			
-			File f = new File("C:/histudy/histudy/src/main/webapp/study-img/"+upload.getOriginalFilename());
-			
-			FileOutputStream fos = new FileOutputStream(f);
-			
-			fos.write(bytes);
-			
-			fos.close();
-		}catch(IOException e) {
-			e.printStackTrace();
-		}
-	}
-	
+   @Autowired
+   private StudyService ss;
+   
+   @GetMapping("/studyList.do")
+   public ModelAndView studyList(@RequestParam(value="cp", defaultValue="1")int cp) {
+      
+      int listSize = 9;
+      int pageSize = 5;
+      int totalCnt = ss.studyTotalCnt();
+      String pagename = "studyList.do";
+      String pageStr = com.histudy.page.PagingModule.makePage(cp, listSize, pageSize, totalCnt, pagename);
+      
+      int start_num = (cp-1)*listSize+1;
+      int end_num = cp*listSize;
+      Map<String, Integer> map = new HashMap<String, Integer>();
+      map.put("start_num", start_num);
+      map.put("end_num", end_num);
+      
+      
+      List<StudyDTO> lists = ss.getStudyList(map);
+      ModelAndView mav = new ModelAndView();
+      mav.addObject("studyList", lists);
+      mav.addObject("studyListCount", lists.size());
+      mav.addObject("pageStr", pageStr);
+      mav.setViewName("study/studyList");
+      return mav;
+   }
+   
+   @GetMapping("/studyCreateForm.do")
+   public ModelAndView studyCreateForm(int user_idx) {
+      UserDTO dto = ss.getStudyCreateUser(user_idx);
+      ModelAndView mav = new ModelAndView();
+      mav.addObject("user_name", dto.getUser_name());
+      mav.addObject("user_tel", dto.getUser_tel());
+      mav.addObject("user_email", dto.getUser_email());
+      mav.setViewName("study/studyCreate");
+      return mav;
+   }
+   
+   @PostMapping("/studyCreate.do")
+   public ModelAndView studyCreate(StudyDTO dto, MultipartFile rstudy_upload_img) {
+      
+      int studyMaxCreateCount = ss.studyMaxCreate(dto.getUser_idx());
+      System.out.println("í˜„ìž¬ ìµœëŒ€ ê°œì„¤ìˆ˜:"+studyMaxCreateCount);
+      System.out.println("ê°œì„¤ìž idx:"+dto.getUser_idx());
+      ModelAndView mav = new ModelAndView();
+      
+      String msg = null;
+      if(studyMaxCreateCount>=3){
+         msg = "ìŠ¤í„°ë”” ê°œì„¤ì€ ìµœëŒ€ 3ê°œê¹Œì§€ ê°€ëŠ¥í•©ë‹ˆë‹¤!";
+         mav.addObject("msg", msg);
+      }else {
+         dto.setStudy_upload_img(rstudy_upload_img.getOriginalFilename());
+         fileCopy(rstudy_upload_img);
+         int result = ss.createStudy(dto);
+         msg = result>0?"ìŠ¤í„°ë”” ê°œì„¤ ì™„ë£Œ":"ìŠ¤í„°ë”” ê°œì„¤ ì‹¤íŒ¨";
+         mav.addObject("msg", msg);
+      }
+      mav.setViewName("study/studyMsg");
+      return mav;
+   }
+   
+   public void fileCopy(MultipartFile upload) {
+      try {
+         byte bytes[] = upload.getBytes();
+         
+         File f = new File("C:/histudy/.metadata/.plugins/org.eclipse.wst.server.core/tmp0/wtpwebapps/histudy/study-img/"+upload.getOriginalFilename());
+         
+         FileOutputStream fos = new FileOutputStream(f);
+         
+         fos.write(bytes);
+         
+         fos.close();
+      }catch(IOException e) {
+         e.printStackTrace();
+      }
+   }
+   
 }
