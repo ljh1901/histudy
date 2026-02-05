36b84ae5dfb0abf4fb7edb6102284d363c35e40d
ljh1901 <juho8690@gmail.com>
2026-02-05 17:32:59 +0900

    Update StudycafeSerivce.java

--- histudy/src/main/java/com/histudy/studycafe/service/StudycafeSerivce.java
+++ histudy/src/main/java/com/histudy/studycafe/service/StudycafeSerivce.java
@@ -1,7 +1,13 @@
 package com.histudy.studycafe.service;
 
+import java.util.List;
+
 import com.histudy.studycafe.model.StudycafeDTO;
+import com.histudy.studycafe.model.StudycafeJoinReservationDTO;
+import com.histudy.studycafe.model.TicketJoinTicketCategoryDTO;
 
 public interface StudycafeSerivce {
-	public StudycafeDTO studycafeList();
+	public List<StudycafeDTO> studycafeList();
+	public StudycafeJoinReservationDTO seatInfo(int seat_idx);
+	public List<TicketJoinTicketCategoryDTO> ticketInfo(int ticket_category_idx);
 }
