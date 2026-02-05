bc18884fbb1e3dd98b23baeab30dd0af78f382b5
ljh1901 <juho8690@gmail.com>
2026-02-05 17:31:17 +0900

    Update StudycafeDAOImple.java

--- histudy/src/main/java/com/histudy/studycafe/model/StudycafeDAOImple.java
+++ histudy/src/main/java/com/histudy/studycafe/model/StudycafeDAOImple.java
@@ -1,5 +1,8 @@
 package com.histudy.studycafe.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.mybatis.spring.SqlSessionTemplate;
 
 public class StudycafeDAOImple implements StudycafeDAO{
@@ -11,10 +14,19 @@ public class StudycafeDAOImple implements StudycafeDAO{
 	}
 	
 	@Override
-	public StudycafeDTO studycafeList() {
-		StudycafeDTO dto =sqlSession.selectOne("studycafeListSQL");
-		return dto;
+	public List<StudycafeDTO> studycafeList() {
+		List<StudycafeDTO> list = sqlSession.selectList("studycafeListSQL");
+		return list;
 	}
-	
+	@Override
+	public StudycafeJoinReservationDTO seatInfo(int seat_idx) {
+		StudycafeJoinReservationDTO seatInfo = sqlSession.selectOne("selectSeatInfoSQL", seat_idx);
+		return seatInfo;
+	}
+	@Override
+	public List<TicketJoinTicketCategoryDTO> ticketInfo(int ticket_category_idx) {
+		List<TicketJoinTicketCategoryDTO> ticketInfo = sqlSession.selectList("selectTicketInfoSQL", ticket_category_idx);
+		return ticketInfo;
+	}
 	
 }
