579de7d1f01feb621c92932069577248b1fc688f
seongji <a7915359@naver.com>
2026-02-09 17:36:44 +0900

    admin 추가

---
+++ histudy/src/main/java/com/histudy/admin/service/AdminReportServiceImple.java
@@ -0,0 +1,31 @@
+package com.histudy.admin.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.histudy.admin.model.AdminReportDAO;
+import com.histudy.admin.model.ReportDTO;
+
+@Service
+public class AdminReportServiceImple implements AdminReportService {
+    @Autowired
+    private AdminReportDAO adminReportDao;
+
+    public AdminReportServiceImple(AdminReportDAO adminReportDao) {
+		super();
+		this.adminReportDao = adminReportDao;
+	}
+
+	@Override
+    public List<ReportDTO> getReportList() {
+        return adminReportDao.selectAllReportList();
+    }
+
+    @Override
+    public int resolveReport(int report_idx) {
+        return adminReportDao.updateReportStatus(report_idx);
+    }
+
+}
