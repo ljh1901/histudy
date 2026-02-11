f3606c04c58b203ba97e1fb3b57bb9cd62c34421
ljh1901 <juho8690@gmail.com>
2026-02-11 11:29:27 +0900

    asd

--- histudy/src/main/java/com/histudy/studycafe/controller/StudyCafePayController.java
+++ histudy/src/main/java/com/histudy/studycafe/controller/StudyCafePayController.java
@@ -6,54 +6,43 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.util.*;
 
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.util.JSONPObject;
+import com.histudy.studycafe.model.PayDTO;
+import com.histudy.studycafe.model.TicketJoinTicketCategoryDTO;
+import com.histudy.studycafe.service.StudycafeSerivce;
+import com.histudy.studycafe.service.StudycafeServiceImple;
 
 @RestController
-@RequestMapping("/studycafe/payment/complete")
+@RequestMapping("/studycafe/payment")
 public class StudyCafePayController {
-	@PostMapping("/studycafe.do")
-	public ResponseEntity<Map<String, String>> payComplete(@RequestBody String paymentId){
-		System.out.println(paymentId);
-		// Service 호출 후
-		try {
-		HttpRequest request = HttpRequest
-				.newBuilder()
-				.uri(URI.create("https://api.portone.io/payments/payment-202621223949?storeId=store-83eacdb9-6d28-4c80-b53b-26d96da03490"))
-				.header("Authorization","PortOne pRHF7tzUF0KgA2pG1KQWOo53nVunQCNYHLkfcagOTGtuPrwb8sShS2MbU2REjgqRjysw5KTuVMmpzyS3")
-				.header("Content-Type", "application/json")
-				.method("GET",HttpRequest.BodyPublishers.ofString("{}"))
-				.build(); 
-		HttpResponse<String> response = HttpClient
-				.newHttpClient()
-				.send(request, HttpResponse
-						.BodyHandlers
-						.ofString()); 
-		 System.out.println(response.body());
-		 
-		 ObjectMapper mapper = new ObjectMapper(); 
-		 JsonNode root = mapper.readTree(response.body());
-		 String customDataStr = root.get("customData").asText();
-		 JsonNode customData = mapper.readTree(customDataStr);
-		 System.out.println(customData.get("storeName").asText());
-
-		 //status, id, method.provider, 
-		 // statusChangedAt(상태값 바뀐 시각), orderName(주문내역), 
-		 //amount.total(총 금액), paid(지불금액), name, email, phoneNumder, customData, paidAt
-		}catch(Exception e) {
-			e.printStackTrace();
-		}
-		Map<String,String> receiptlist = new HashMap<String, String>();
-		ResponseEntity<Map<String,String>> receipt = new ResponseEntity<Map<String,String>>(receiptlist, HttpStatus.OK);
-		return receipt;
+	
+	@Autowired
+	private StudycafeSerivce studycafeService;
+	
+	@PostMapping(value="/payNotComplete.do")
+	public ResponseEntity<PayDTO> payNotComplete(@RequestBody PayDTO payOrder, HttpSession session, @RequestParam int ticket_idx){
+		payOrder.setUser_idx((Integer) session.getAttribute("user_idx"));
+		int result = studycafeService.orderPay(payOrder);
+		TicketJoinTicketCategoryDTO tjtcdto = studycafeService.ticketTotalAmount(ticket_idx);
+		PayDTO payRequest = result>0?payOrder:null;
+		payRequest.setTotalAmount(tjtcdto.getTicket_amount());
+		ResponseEntity<PayDTO> re = new ResponseEntity<PayDTO>(payRequest, HttpStatus.OK);
+		return re;
 	}
 }
--- histudy/src/main/webapp/WEB-INF/views/header.jsp
+++ histudy/src/main/webapp/WEB-INF/views/header.jsp
@@ -1,8 +1,7 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
 	pageEncoding="UTF-8"%>
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
-<link rel="stylesheet" type="text/css"
-	href="${pageContext.request.contextPath}/css/mypage.css">
+<link rel="stylesheet" type="text/css" href="${pageContext.request.contextPath}/css/mypage.css">
 <header class="header">
 	<div class="header__left">
 		<img src="/histudy/main-img/logo1.png" class="header__logo">
@@ -15,17 +14,14 @@
 			<li class="header__nav__menu__item"><a href="index.do">Home</a></li>
 			<li class="header__nav__menu__item"><a href="studyList.do">Study</a></li>
 			<li class="header__nav__menu__item"><a href="mentorList.do">Mentoring</a></li>
-			<li class="header__nav__menu__item"><a href="studycafe.do">Study
-					Cafe</a></li>
+			<li class="header__nav__menu__item"><a href="studycafe.do">StudyCafe</a></li>
 			<li class="header__nav__menu__item"><a href="#">LMS</a></li>
 			<li class="header__nav__menu__item"><a href="lecture.do">Lecture</a></li>
 		</ul>
 	</nav>
-
 	<div class="header__right">
     <c:if test="${empty sessionScope.user_id}">
-        <input type="button" value="Login" class="header__login"
-            onclick="openSignInModal()">
+        <input type="button" value="Login" class="header__login" onclick="openSignInModal()">
     </c:if>
 
     <c:if test="${not empty sessionScope.user_id}">
