2b833d7760869e93ddc494c45a258b99e1f81926
ljh1901 <juho8690@gmail.com>
2026-02-13 16:13:42 +0900

    ss

--- histudy/src/main/java/com/histudy/studycafe/model/PayDTO.java
+++ histudy/src/main/java/com/histudy/studycafe/model/PayDTO.java
@@ -1,5 +1,8 @@
 package com.histudy.studycafe.model;
 
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+
 public class PayDTO {
 	private String paymentId;
 	private String storeId;
@@ -9,8 +12,8 @@ public class PayDTO {
 	private int user_idx;
 	private String pay_method;
 	private String pay_status;
-	private String statusChangedAt;
-	private String paidAt;
+	private Timestamp statusChangedAt;
+	private Timestamp paidAt;
 	private int totalAmount;
 	private int supply;
 	private int vat;
@@ -18,13 +21,14 @@ public class PayDTO {
 	public PayDTO() {
 		super();
 	}
-
-	public PayDTO(String paymentId, String storeId, String orderName, int paid, int user_idx, String pay_method,
-			String pay_status, String statusChangedAt, String paidAt, Integer totalAmount,
-			String channelKey, String pgProvider, int supply, int vat) {
+	
+	public PayDTO(String paymentId, String storeId, String channelKey, String orderName, int paid, int user_idx,
+			String pay_method, String pay_status, Timestamp statusChangedAt, Timestamp paidAt, int totalAmount,
+			int supply, int vat, String pgProvider) {
 		super();
 		this.paymentId = paymentId;
 		this.storeId = storeId;
+		this.channelKey = channelKey;
 		this.orderName = orderName;
 		this.paid = paid;
 		this.user_idx = user_idx;
@@ -33,46 +37,9 @@ public class PayDTO {
 		this.statusChangedAt = statusChangedAt;
 		this.paidAt = paidAt;
 		this.totalAmount = totalAmount;
-		this.channelKey = channelKey;
-		this.pgProvider = pgProvider;
 		this.supply = supply;
 		this.vat = vat;
-	}
-
-	public int getSupply() {
-		return supply;
-	}
-
-	public void setSupply(int supply) {
-		this.supply = supply;
-	}
-
-	public int getVat() {
-		return vat;
-	}
-
-	public void setVat(int vat) {
-		this.vat = vat;
-	}
-
-	public String getPgProvider() {
-		return pgProvider;
-	}
-
-	public void setPgProvider(String pgProvider) {
 		this.pgProvider = pgProvider;
-	}
-
-	public void setTotalAmount(int totalAmount) {
-		this.totalAmount = totalAmount;
-	}
-
-	public String getChannelKey() {
-		return channelKey;
-	}
-
-	public void setChannelKey(String channelKey) {
-		this.channelKey = channelKey;
 	}
 
 	public String getPaymentId() {
@@ -81,75 +48,87 @@ public class PayDTO {
 	public void setPaymentId(String paymentId) {
 		this.paymentId = paymentId;
 	}
+	public String getStoreId() {
+		return storeId;
+	}
+	public void setStoreId(String storeId) {
+		this.storeId = storeId;
+	}
+	public String getChannelKey() {
+		return channelKey;
+	}
+	public void setChannelKey(String channelKey) {
+		this.channelKey = channelKey;
+	}
 	public String getOrderName() {
 		return orderName;
 	}
 	public void setOrderName(String orderName) {
 		this.orderName = orderName;
 	}
-	public Integer getTotalAmount() {
-		return totalAmount;
-	}
-	public void setTotalAmount(Integer totalAmount) {
-		this.totalAmount = totalAmount;
-	}
-
-	public String getStoreId() {
-		return storeId;
-	}
-
-	public void setStoreId(String storeId) {
-		this.storeId = storeId;
-	}
-
 	public int getPaid() {
 		return paid;
 	}
-
 	public void setPaid(int paid) {
 		this.paid = paid;
 	}
-
-
 	public int getUser_idx() {
 		return user_idx;
 	}
-
 	public void setUser_idx(int user_idx) {
 		this.user_idx = user_idx;
 	}
-
 	public String getPay_method() {
 		return pay_method;
 	}
-
 	public void setPay_method(String pay_method) {
 		this.pay_method = pay_method;
 	}
-
 	public String getPay_status() {
 		return pay_status;
 	}
-
 	public void setPay_status(String pay_status) {
 		this.pay_status = pay_status;
 	}
-
-	public String getStatusChangedAt() {
+	public Timestamp getStatusChangedAt() {
 		return statusChangedAt;
 	}
-
-	public void setStatusChangedAt(String statusChangeAt) {
-		this.statusChangedAt = statusChangeAt;
+	public void setStatusChangedAt(Timestamp statusChangedAt) {
+		this.statusChangedAt = statusChangedAt;
 	}
-
-	public String getPaidAt() {
+	public Timestamp getPaidAt() {
 		return paidAt;
 	}
-
-	public void setPaidAt(String paidAt) {
+	public void setPaidAt(Timestamp paidAt) {
 		this.paidAt = paidAt;
 	}
+	public int getTotalAmount() {
+		return totalAmount;
+	}
+	public void setTotalAmount(int totalAmount) {
+		this.totalAmount = totalAmount;
+	}
+	public int getSupply() {
+		return supply;
+	}
+	public void setSupply(int supply) {
+		this.supply = supply;
+	}
+	public int getVat() {
+		return vat;
+	}
+	public void setVat(int vat) {
+		this.vat = vat;
+	}
+	public String getPgProvider() {
+		return pgProvider;
+	}
+	public void setPgProvider(String pgProvider) {
+		this.pgProvider = pgProvider;
+	}
+	
+
+
 	
 	
 }
